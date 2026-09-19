4ea8127f921c27f4ef3fee3361b6f8b43098d84d
Kriest van Lokven <kriesje10@hotmail.com>
2026-09-19 20:38:09 +0200

    Log error bugfix

---
+++ RemoteRigsRaspberryPi/dist/src/src/common/debug.js
@@ -0,0 +1,75 @@
+import { LogModel, LogSettingType, StatusModel, StatusType } from '../models/models.js';
+import Server from "../server.js";
+import SignalR from "./signalr.js";
+export default class Debug {
+    // Timings
+    static { this.watchdogtimer = 1000; }
+    static { this.pwmdelay = 100; }
+    static { this.pwmstart = 120; }
+    static { this.pwm_frequency = 800; }
+    static { this.pwm_dutycycle_max = 1000000; }
+    static { this.time_to_full_speed_ms = 10000; }
+    static { this.commandtimer = 500; }
+    static SetErrorCallback(callback_param) {
+        Debug.error_callback = callback_param;
+    }
+    static Log(pVehicleSettingType, id, name, message) {
+        if (Debug.GetLogSetting(pVehicleSettingType, id)) {
+            Debug.LogAlways(name, message, true);
+        }
+    }
+    static LogAlways(name, message, signalr) {
+        var date = new Date();
+        if (signalr) {
+            var logModel = new LogModel();
+            logModel.dateTime = date;
+            logModel.name = name;
+            logModel.message = message;
+            Debug.SendLogModel(logModel);
+        }
+        console.log(date.toLocaleTimeString() + " / " + name + " / " + message);
+    }
+    static Error(name, message) {
+        var date = new Date();
+        var logModel = new LogModel();
+        logModel.dateTime = date;
+        logModel.name = name;
+        logModel.message = message;
+        Debug.SendLogModel(logModel);
+        console.error(date.toLocaleTimeString() + " / " + name + " / " + message);
+        Debug.error_callback();
+    }
+    static SendLogModel(logModel) {
+        var tStatusModel = new StatusModel();
+        tStatusModel.type = StatusType.RigLog;
+        tStatusModel.value = logModel;
+        SignalR.UpdateStatus(tStatusModel);
+    }
+    static GetLogSetting(pLogSettingType, id) {
+        if (Server.rig != null) {
+            for (var i = 0; i < Server.rig.rig.logSettings.length; i++) {
+                if (Server.rig.rig.logSettings[i].logSettingType == pLogSettingType &&
+                    Server.rig.rig.logSettings[i].parameter1 == id) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static GetStringValue(logSettingType) {
+        switch (logSettingType) {
+            case LogSettingType.BLESensor:
+                return "BLESensor";
+            case LogSettingType.Commands:
+                return "Commands";
+            case LogSettingType.KeyEvents:
+                return "KeyEvents";
+            case LogSettingType.SignalR:
+                return "SignalR";
+            case LogSettingType.Component:
+                return "Component";
+        }
+        return "";
+    }
+}
+//# sourceMappingURL=debug.js.map
--- RemoteRigsRaspberryPi/dist/src/src/common/signalr.js
+++ RemoteRigsRaspberryPi/dist/src/src/common/signalr.js
@@ -1,33 +1,26 @@
-﻿import * as signalR from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
 import Server from './../server.js';
 import Debug from './debug.js';
 import Rig from '../rig.js';
 import { exec } from "child_process";
-import { KeyEventClientModel, LogSettingType, RigClientViewModel, WebRTCMessage, HighScoreModel, CommandMessage, CommandType, StatusModel, StatusType } from '../models/models.js';
-
+import { LogSettingType, CommandType, StatusType } from '../models/models.js';
 export default class SignalR {
-    private static hubConnection: signalR.HubConnection | null = null;
-    private static reconnectTimer: NodeJS.Timeout | null = null;
-    private static lastUniqueCode: string | null = null;
-    private static intentionalStop: boolean = false;
-
-    private static GetName(): string {
+    static { this.hubConnection = null; }
+    static { this.reconnectTimer = null; }
+    static { this.lastUniqueCode = null; }
+    static { this.intentionalStop = false; }
+    static GetName() {
         return "SignalR";
     }
-
-    public static StartConnection(uniquecode: string): void {
-
+    static StartConnection(uniquecode) {
         SignalR.lastUniqueCode = uniquecode;
         SignalR.intentionalStop = false;
-
         Debug.LogAlways(SignalR.GetName(), "initiating hub connection... url: '" + Server.signalr + "'", false);
-
         SignalR.hubConnection = new signalR.HubConnectionBuilder()
             .withUrl(Server.signalr)
             .withAutomaticReconnect()
             .build();
-
-        SignalR.hubConnection.on('keyevent', (keyEventClientModel: KeyEventClientModel) => {
+        SignalR.hubConnection.on('keyevent', (keyEventClientModel) => {
             if (Server.rig != null) {
                 Server.rig.ProcessKeyEvent(keyEventClientModel);
             }
@@ -35,13 +28,11 @@ export default class SignalR {
                 Debug.LogAlways(SignalR.GetName(), "no rig to process keyevent", true);
             }
         });
-
-        SignalR.hubConnection.on("web-rtc-message", async (webRTCMessage: WebRTCMessage) => {
+        SignalR.hubConnection.on("web-rtc-message", async (webRTCMessage) => {
             if (Server.rig != null) {
                 if (Server.rig.rig.id !== webRTCMessage.rigId) {
                     Debug.Error(SignalR.GetName(), "incoming web-rtc-message error Server.rig.rig.id (" + Server.rig.rig.id + ") !== message.rigId (" + webRTCMessage.rigId + ")");
                 }
-
                 Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "incoming web-rtc-message rigId:" + webRTCMessage.rigId + " componentId:" + webRTCMessage.componentId + " userName: " + webRTCMessage.userName + " rigOwnerUsername: " + webRTCMessage.rigOwnerUsername);
                 Server.rig.PutWebRTCMessage(webRTCMessage);
             }
@@ -49,10 +40,8 @@ export default class SignalR {
                 Debug.LogAlways(SignalR.GetName(), "no rig to process webrtc message", true);
             }
         });
-
-        SignalR.hubConnection.on("send-command", async (commandMessage: CommandMessage) => {
+        SignalR.hubConnection.on("send-command", async (commandMessage) => {
             Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "incoming message: " + JSON.stringify(commandMessage));
-
             switch (commandMessage.type) {
                 case CommandType.RigReloadRigModel:
                     SignalR.SignIn(uniquecode);
@@ -63,159 +52,138 @@ export default class SignalR {
                     break;
             }
         });
-
         SignalR.hubConnection.onreconnecting((error) => {
             Debug.LogAlways(SignalR.GetName(), "hub connection lost, attempting to reconnect..." + (error ? (" reason: " + error) : ""), true);
         });
-
         SignalR.hubConnection.onreconnected(() => {
             Debug.LogAlways(SignalR.GetName(), "hub connection reconnected, re-signing in...", true);
             SignalR.SignIn(uniquecode);
         });
-
         SignalR.hubConnection.onclose((error) => {
             Debug.LogAlways(SignalR.GetName(), "hub connection closed" + (error ? (": " + error) : ""), true);
             SignalR.ScheduleReconnect();
         });
-
         SignalR.hubConnection
             .start()
             .then(() => {
-                Debug.LogAlways(SignalR.GetName(), 'hub connection established', false);
-                SignalR.SignIn(uniquecode);
-            })
+            Debug.LogAlways(SignalR.GetName(), 'hub connection established', false);
+            SignalR.SignIn(uniquecode);
+        })
             .catch((err) => {
-                Debug.LogAlways(SignalR.GetName(), "failed to establish hub connection: " + err, false);
-                SignalR.ScheduleReconnect();
-            });
+            Debug.LogAlways(SignalR.GetName(), "failed to establish hub connection: " + err, false);
+            SignalR.ScheduleReconnect();
+        });
     }
-
-    private static ScheduleReconnect(): void {
-        if (SignalR.intentionalStop) {1
+    static ScheduleReconnect() {
+        if (SignalR.intentionalStop) {
+            1;
             return;
         }
-
         if (SignalR.reconnectTimer != null) {
             return;
         }
-
         Debug.LogAlways(SignalR.GetName(), "scheduling reconnect attempt in 30s...", true);
-
         SignalR.reconnectTimer = setTimeout(() => {
             SignalR.reconnectTimer = null;
-
             if (SignalR.lastUniqueCode != null && !SignalR.intentionalStop) {
                 Debug.LogAlways(SignalR.GetName(), "attempting reconnect now...", true);
                 SignalR.StartConnection(SignalR.lastUniqueCode);
             }
         }, 30000);
     }
-
-    public static StopConnection(): void {
+    static StopConnection() {
         Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "stopping hub connection...");
-
         SignalR.intentionalStop = true;
-
         if (SignalR.reconnectTimer != null) {
             clearTimeout(SignalR.reconnectTimer);
             SignalR.reconnectTimer = null;
         }
-
         if (SignalR.hubConnection) {
             SignalR.hubConnection.stop()
                 .then(() => {
-                    Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), 'hub connection stopped');
-                })
+                Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), 'hub connection stopped');
+            })
                 .catch((err) => {
-                    Debug.Error(SignalR.GetName(), err);
-                });
+                Debug.Error(SignalR.GetName(), err);
+            });
         }
     }
-
-    public static IsConnected(): boolean {
+    static IsConnected() {
         return SignalR.hubConnection != null && SignalR.hubConnection.state === signalR.HubConnectionState.Connected;
     }
-
-    public static SignIn(uniquecode: string): void {
+    static SignIn(uniquecode) {
         if (SignalR.hubConnection != null && SignalR.IsConnected()) {
             SignalR.hubConnection.invoke('SignIn', uniquecode)
-                .then((rigClientViewModel: RigClientViewModel) => {
-                    if (rigClientViewModel.rigModelViewModel == undefined) {
-                        Debug.LogAlways(SignalR.GetName(), "sign in to server failed! GUID: " + uniquecode, true);
-                    }
-                    else {
-                        Debug.LogAlways(SignalR.GetName(), "sign in to server successful", true);
-                        Debug.LogAlways(SignalR.GetName(), "rig #" + rigClientViewModel.id + " '" + rigClientViewModel.name + "'", true);
-                        Debug.LogAlways(SignalR.GetName(), "rig model #" + rigClientViewModel.rigModelViewModel.id + " '" + rigClientViewModel.rigModelViewModel.name + "'", true);
-
-                        Server.rig = new Rig(rigClientViewModel);
-
-                        Server.rig.CreateComponents();
-                        Server.rig.Init();
-                        Server.rig.SendCurrent();
-                    }
-                })
+                .then((rigClientViewModel) => {
+                if (rigClientViewModel.rigModelViewModel == undefined) {
+                    Debug.LogAlways(SignalR.GetName(), "sign in to server failed! GUID: " + uniquecode, true);
+                }
+                else {
+                    Debug.LogAlways(SignalR.GetName(), "sign in to server successful", true);
+                    Debug.LogAlways(SignalR.GetName(), "rig #" + rigClientViewModel.id + " '" + rigClientViewModel.name + "'", true);
+                    Debug.LogAlways(SignalR.GetName(), "rig model #" + rigClientViewModel.rigModelViewModel.id + " '" + rigClientViewModel.rigModelViewModel.name + "'", true);
+                    Server.rig = new Rig(rigClientViewModel);
+                    Server.rig.CreateComponents();
+                    Server.rig.Init();
+                    Server.rig.SendCurrent();
+                }
+            })
                 .catch((err) => {
-                    Debug.Error(SignalR.GetName(), err);
-                });
+                Debug.Error(SignalR.GetName(), err);
+            });
         }
         else {
             Debug.Error(SignalR.GetName(), "sign in canceled: no hub connection");
         }
     }
-
-    public static UpdateStatus(pStatusModel: StatusModel): void {
+    static UpdateStatus(pStatusModel) {
         if (SignalR.hubConnection != null && SignalR.IsConnected()) {
             if (pStatusModel.type != StatusType.RigLog) {
                 Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing UpdateStatus type: " + pStatusModel.type);
             }
-
             SignalR.hubConnection.invoke('UpdateStatus', pStatusModel)
                 .then(() => {
-                    if (pStatusModel.type != StatusType.RigLog) {
-                        Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing UpdateStatus type: " + pStatusModel.type);
-                    }
-                })
+                if (pStatusModel.type != StatusType.RigLog) {
+                    Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing UpdateStatus type: " + pStatusModel.type);
+                }
+            })
                 .catch((err) => {
-                    Debug.Error(SignalR.GetName(), "outgoing UpdateStatus error: " + err);
-                });
+                Debug.Error(SignalR.GetName(), "outgoing UpdateStatus error: " + err);
+            });
         }
         else {
             Debug.Error(SignalR.GetName(), "outgoing UpdateStatus: no hub connection");
         }
     }
-
-    public static SendWebRTCMessage(message: WebRTCMessage): void {
+    static SendWebRTCMessage(message) {
         if (SignalR.hubConnection != null && SignalR.IsConnected()) {
             Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing SendWebRTCMessage rigId: " + message.rigId + " componentId:" + message.componentId + " userName: " + message.userName + " rigOwnerUsername: " + message.rigOwnerUsername);
-
             SignalR.hubConnection.invoke("SendWebRTCMessage", message)
                 .then(() => {
-                    Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing SendWebRTCMessage success rigId: " + message.rigId + " componentId:" + message.componentId + " userName: " + message.userName + " rigOwnerUsername: " + message.rigOwnerUsername);
-                })
+                Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing SendWebRTCMessage success rigId: " + message.rigId + " componentId:" + message.componentId + " userName: " + message.userName + " rigOwnerUsername: " + message.rigOwnerUsername);
+            })
                 .catch((err) => {
-                    Debug.Error(SignalR.GetName(), "outgoing SendWebRTCMessage error: " + err);
-                });
+                Debug.Error(SignalR.GetName(), "outgoing SendWebRTCMessage error: " + err);
+            });
         }
         else {
             Debug.Error(SignalR.GetName(), "SendWebRTCMessage: no hub connection");
         }
     }
-
-    public static AddHighScorePoints(message: HighScoreModel): void {
+    static AddHighScorePoints(message) {
         if (SignalR.hubConnection != null && SignalR.IsConnected()) {
             Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing AddHighScorePoints: " + JSON.stringify(message));
-
             SignalR.hubConnection.invoke("AddHighScorePoints", message)
                 .then(() => {
-                    Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing AddHighScorePoints success: " + JSON.stringify(message));
-                })
+                Debug.Log(LogSettingType.SignalR, undefined, SignalR.GetName(), "outgoing AddHighScorePoints success: " + JSON.stringify(message));
+            })
                 .catch((err) => {
-                    Debug.Error(SignalR.GetName(), "outgoing AddHighScorePoints error: " + err);
-                });
+                Debug.Error(SignalR.GetName(), "outgoing AddHighScorePoints error: " + err);
+            });
         }
         else {
             Debug.Error(SignalR.GetName(), "outgoing AddHighScorePoints: no hub connection");
         }
     }
 }
+//# sourceMappingURL=signalr.js.map
